5124e9d24754e4e226817dd04d19b0048e00bf08
Eduardopatriota <39865404+Eduardopatriota@users.noreply.github.com>
2020-02-06 21:39:58 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var nome = 1;
+function sumirFoto(el) {
+    
+    if(el = true && nome == 1){
+    document.getElementById('sumir').id = 'aparecer' 
+    nome++;
+    console.log(nome); }
+    else  {
+    document.getElementById('aparecer').id = 'sumir' ; 
+    nome--
+    }
+}
+
+
